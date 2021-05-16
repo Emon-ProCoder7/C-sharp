974343487d9900b2f655be92fa71f264bf6dcba2
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2021-05-16 23:19:11 +0600

    Create ASCII.cs

---
+++ ASCII.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // This code is for ASCII art.
+      Console.WriteLine(" .-."); 
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");  
+      Console.WriteLine("'~~~'");  
+      Console.WriteLine("Spiro the Specter");
+      Console.WriteLine("\n");
+      Console.WriteLine("c_     ___"); 
+      Console.WriteLine("　 \\\\__|__|__"); 
+      Console.WriteLine("　   \\( o_o)");
+      Console.WriteLine("　  　 > ~  >");  
+      Console.WriteLine("　　　/ 　 / \\\\");  
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/"); 
+      Console.WriteLine("　  /　 /"); 
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\");  
+      Console.WriteLine("  |　|  \\");  
+      Console.WriteLine("　| / \\  )");  
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  ) (_/"); 
+      Console.WriteLine("(_ /");
+      
+     }
+  }
+}
