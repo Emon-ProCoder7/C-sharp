cf57d6bb2d7c44bc7b0791348dab5c974da12985
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2021-05-20 08:17:51 +0600

    Create wordArray.cs

---
+++ wordArray.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+            
+            //your code goes here
+            foreach(string x in words){
+                if(x.Contains(letter)){
+                    Console.WriteLine(x);
+                    count++;
+                } else if (count == 0 && x == words[words.Length - 1]){
+                    Console.WriteLine("No match");
+                }
+            }
+            
+        }
+    }
+}
